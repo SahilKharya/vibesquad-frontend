ebe6ceb42091c06d9e38e2e8e771909ad9ef442b
Sahil Kharya <kharyasahil@gmail.com>
2024-10-10 01:05:13 -0400

    Twitter Working Auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ import React from "react";
 export default async function Home() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-purple-500 via-pink-500 to-red-500 w-full">
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r white from-blue-100  to-teal-100 w-full">
         <h1
-          className="opacity-0 animate-fade-in bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 bg-clip-text text-center font-display text-5xl font-bold tracking-tight text-transparent drop-shadow-lg md:text-6xl md:leading-tight"
+          className="opacity-0 animate-fade-in bg-gradient-to-r from-blue-400 to-blue-500 bg-clip-text text-center font-display text-5xl font-bold tracking-tight text-transparent drop-shadow-lg md:text-6xl md:leading-tight"
           style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
         >
           Elevate Your Influence.
         </h1>
-        <p className="mt-4 text-center text-lg text-white md:text-xl">
+        <p className="mt-4 text-center text-lg text-gray-800 md:text-xl">
           Join us to enhance your social presence and connect with like-minded
           individuals.
         </p>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ export default function NavBar() {
 
   return (
     // <div className=' bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0'>
-    <div className="bg-red-200 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
+    <div className="bg-red-400 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
       <div className="mx-5 flex h-16 max-w-screen-xl items-center justify-between w-full">
         <Link href="/" className="flex items-center font-display text-2xl">
           {/* <Image
